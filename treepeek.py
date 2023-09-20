266b5f12bd97588d0471e86fa89daae4e95cd27b
斥疑浮藤 <chifunt@pm.me>
2023-09-20 20:15:08 +0100

    feat: implement copy button

--- treepeek.py
+++ treepeek.py
@@ -21,12 +21,27 @@ def show_structure():
     structure = print_dir_structure(dir_path)
     txt_display.delete(1.0, tk.END)
     txt_display.insert(tk.END, structure)
+    btn_copy.configure(state=tk.NORMAL)
+
+def copy_to_clipboard():
+    content = txt_display.get(1.0, tk.END)
+    app.clipboard_clear()
+    app.clipboard_append(content)
 
 app = tk.Tk()
 app.title("Directory Structure Viewer")
 
-btn_select_dir = ctk.CTkButton(app, text="Select Directory", command=show_structure)
-btn_select_dir.pack(pady=(20, 0))
+# Frame to hold both buttons
+frame_buttons = tk.Frame(app)
+frame_buttons.pack(pady=(20, 0))
+
+# Select Directory button
+btn_select_dir = ctk.CTkButton(frame_buttons, text="Select Directory", command=show_structure)
+btn_select_dir.grid(row=0, column=0, padx=(0, 10))  # Some padding between the buttons
+
+# Copy button
+btn_copy = ctk.CTkButton(frame_buttons, text="Copy", command=copy_to_clipboard, state=tk.DISABLED)
+btn_copy.grid(row=0, column=1)
 
 # Frame to hold the Text widget and its Scrollbar
 frame_txt_display = tk.Frame(app)
@@ -40,6 +55,6 @@ scrollbar = tk.Scrollbar(frame_txt_display, command=txt_display.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
 # Link the scrollbar to the txt_display widget
-txt_display.config(yscrollcommand=scrollbar.set)
+txt_display.configure(yscrollcommand=scrollbar.set)
 
 app.mainloop()
