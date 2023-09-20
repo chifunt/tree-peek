3d23dbe3b1e4e5ee48dd5a50ae4e31d454b73b9a
斥疑浮藤 <chifunt@pm.me>
2023-09-20 19:58:59 +0100

    feat: implement scrollbar

--- treepeek.py
+++ treepeek.py
@@ -28,7 +28,18 @@ app.title("Directory Structure Viewer")
 btn_select_dir = ctk.CTkButton(app, text="Select Directory", command=show_structure)
 btn_select_dir.pack(pady=20)
 
-txt_display = tk.Text(app, wrap=tk.WORD, width=80, height=20)
-txt_display.pack(padx=20, pady=20)
+# Frame to hold the Text widget and its Scrollbar
+frame_txt_display = tk.Frame(app)
+frame_txt_display.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
+
+txt_display = tk.Text(frame_txt_display, wrap=tk.WORD, width=80, height=20)
+txt_display.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+# Vertical Scrollbar for txt_display
+scrollbar = tk.Scrollbar(frame_txt_display, command=txt_display.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Link the scrollbar to the txt_display widget
+txt_display.config(yscrollcommand=scrollbar.set)
 
 app.mainloop()
